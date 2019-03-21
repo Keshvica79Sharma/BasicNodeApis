b7673ac8cab8548fe82d433d4ff12b44416a365b
Keshvica Sharma <k0s01ix@homeoffice.wal-mart.com>
2019-03-21 21:02:03 +0530

    add to cart

--- index.js
+++ index.js
@@ -181,22 +181,6 @@ function searchHistoryOrTopItems(category, filterRecordsPerCategory) {
     console.dir(topThreeMatches[0]);
 
     return {
-             "fulfillmentText": "This is a text response",
-             "fulfillmentMessages": [
-               {
-                 "card": {
-                   "title": "card title",
-                   "subtitle": "card text",
-                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                   "buttons": [
-                     {
-                       "text": "button text",
-                       "postback": "https://assistant.google.com/"
-                     }
-                   ]
-                 }
-               }
-             ],
              "source": "example.com",
              "payload": {
                             "google": {
