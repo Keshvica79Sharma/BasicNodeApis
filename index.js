86baf3e6d9a2f062f8db567804d85575f6987af2
Keshvica Sharma <k0s01ix@homeoffice.wal-mart.com>
2019-03-21 16:36:28 +0530

    add to cart

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var _ = require("underscore");
 var Request = require("request");
+const https = require('https')
 
 const restService = express();
 
@@ -18,19 +19,19 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/selectAppropriateItemAndPlaceOrder", function(req, res) {
+restService.post("/selectAppropriateItemOrPlaceOrder", function(req, res) {
   if(!req.body) return res.sendStatus(400);
+
   res.setHeader('Content-Type', 'application/json');
+
   var responseObj = undefined;
 
   var category = req.body.queryResult.parameters['ItemCategory'];
-
   var subCategory = req.body.queryResult.parameters['SubCategory'];
-  var subCategoryDiet = "diet";
-
   var subSubCategory = req.body.queryResult.parameters['SubSubCategory'];
+  if(!category) return res.sendStatus(400);
 
-  // Searching from top 50 items
+  // Searching from top 30 items
   var url = "https://www.samsclub.com/api/node/vivaldi/v1/products/search/?sourceType=1&selectedFilter=all&sortKey=relevance&sortOrder=1&offset=0&limit=30&searchTerm=" + category + "&clubId=6612";
 
   Request.get(url, (error, response, body) => {
@@ -66,91 +67,226 @@ restService.post("/selectAppropriateItemAndPlaceOrder", function(req, res) {
 //
 //            console.dir(productNameToIdMap);
 
-            var filterRecordsPerSubCategories = undefined;
-            if(subCategory.toLowerCase() == subCategoryDiet) {
-              filterRecordsPerSubCategories = _.filter(filterRecordsPerCategory, function(record) {
+            if(subSubCategory != undefined && subCategory != undefined) {
+                    /*
+                    Category, sub category and sub sub category is defined, lets find the match and add to cart
+                    */
+
+                    var filterRecordsPerSubCategories = undefined;
+                    var subCategoryDiet = "diet";
+
+                    if(subCategory.toLowerCase() == subCategoryDiet) {
+                        filterRecordsPerSubCategories = _.filter(filterRecordsPerCategory, function(record) {
+                        var productName = record.productName.toLowerCase();
+                        return productName.includes(subCategoryDiet) && productName.includes(subSubCategory.toLowerCase());
+                        });
+                    } else {
+                          filterRecordsPerSubCategories = _.filter(filterRecordsPerCategory, function(record) {
                           var productName = record.productName.toLowerCase();
-                          return productName.includes(subCategoryDiet) && productName.includes(subSubCategory.toLowerCase());
-                    });
-            } else {
-              filterRecordsPerSubCategories = _.filter(filterRecordsPerCategory, function(record) {
-                                      var productName = record.productName.toLowerCase();
-                                      return !productName.includes(subCategoryDiet) && productName.includes(subSubCategory.toLowerCase());
-                    });
-            }
+                          return !productName.includes(subCategoryDiet) && productName.includes(subSubCategory.toLowerCase());
+                          });
+                    }
 
 
-//            var blah = _.object(_.map(filterRecordsPerSubCategories, function(record) {
-//                               return [record.productName, record.productId]
-//                            }));
-//
-//            console.dir(blah);
+            //            var blah = _.object(_.map(filterRecordsPerSubCategories, function(record) {
+            //                               return [record.productName, record.productId]
+            //                            }));
+            //
+            //            console.dir(blah);
 
 
-            if(filterRecordsPerSubCategories.length == 0) {
-              responseObj = {
-                  "fulfillmentText": "Sorry, could not find a matching item for your request",
-                  "fulfillmentMessages": [
-                      {
-                          "text": {
-                              "text": ["Sorry, could not find a matching item for your request"]
-                          }
-                      }
-                      ],
-                      "source": "hackday-service.herokuapp.com"
-                      }
-            } else {
-                var itemToOrder = filterRecordsPerSubCategories[0];
-                responseObj = {
-                              "fulfillmentText": "Sure, Added " + itemToOrder.productName + " to your cart",
+                    if(filterRecordsPerSubCategories.length == 0) {
+                        responseObj = {
+                        "fulfillmentText": "Sorry, could not find a matching item for your request",
                               "fulfillmentMessages": [
                                   {
                                       "text": {
-                                          "text": ["Sure, Added " + itemToOrder.productName + " to your cart"]
+                                          "text": ["Sorry, could not find a matching item for your request"]
                                       }
                                   }
                                   ],
                                   "source": "hackday-service.herokuapp.com"
                                   }
+                    } else {
+                        var itemToOrder = filterRecordsPerSubCategories[0];
+                        var productId = itemToOrder.productId;
+                        var skuId = itemToOrder.skuOptions[0].skuId;
+
+                        var body = {"payload":{"items":[{"quantity":1,"channel":"club","offerId":{"USItemId": productId,"USVariantId": skuId,"USSellerId":0}}]}};
+
+
+                        // Set the headers
+                        var headers = {
+                            'Content-Type': 'application/json',
+                            'scheme':'https',
+                            'accept-encoding':'gzip, deflate, br',
+                            'accept-language':'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
+                            'origin':'https://www.samsclub.com',
+                            'cookie': 'JSESSIONID=8B45821873B9EBF74EE9D481D44F2162.estoreapp-44277956-16-70642835'}
+
+                        // Configure the request
+                        var options = {
+                            url: 'https://www.samsclub.com/api/node/cartservice/v1/carts/800f6c9d685e3dbb7b5442f101781d2d/cartitems?response_groups=cart.medium',
+                            port: 443,
+                            method: 'POST',
+                            headers: headers,
+                            body: JSON.stringify(body)
+                        }
+
+                        // Start the request
+                        Request(options, function (error, response, body) {
+                            if (!error && response.statusCode == 200) {
+                                // Print out the response body
+                                console.log("****SUCESS");
+                                console.log(body)
+                            } else {
+                                console.log("****fail");
+                                console.log(error);
+                            }
+                        })
+
+                        responseObj = {
+                        "fulfillmentText": "Sure, Added " + itemToOrder.productName + " to your cart",
+                        "fulfillmentMessages": [
+                                {
+                                    "text": {
+                                            "text": ["Sure, Added " + itemToOrder.productName + " to your cart"]
+                                    }
+                                }
+                                ],
+                                "source": "hackday-service.herokuapp.com"
+                            }
+                    }
+            } else {
+                /*
+                Only category and sub category is defined, lets pull out history or top 5 items
+                */
+                responseObj = searchHistoryOrTopItems(category, filterRecordsPerCategory);
+
             }
        }
-      console.dir(responseObj);
       return res.json(responseObj);
   });
 });
 
-restService.post("/getFiveTopRatedItems", function(req, res) {
-  if(!req.body) return res.sendStatus(400);
-  res.setHeader('Content-Type', 'application/json');
+function searchHistoryOrTopItems(category, filterRecordsPerCategory) {
+    var itemFlag = "TopRated";
+    var topMatches = _.filter(filterRecordsPerCategory, function(record) {
+        return record.itemFlag == itemFlag;
+    });
+    var topFiveMatches = _.filter(topMatches, function(record) {
+            return topMatches.indexOf(record) < 5;
+    });
 
-  var category = req.body.queryResult.parameters['ItemCategory'];
+    return {
+             "fulfillmentText": "Sure Thing. Here is a list of top rated items for " + category,
+             "fulfillmentMessages": [
+               {
+                 "card": {
+                   "title": "card title",
+                   "subtitle": "card text",
+                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                   "buttons": [
+                     {
+                       "text": "button text",
+                       "postback": "https://assistant.google.com/"
+                     }
+                   ]
+                 }
+               },
+               {
+                 "card": {
+                   "title": "card title",
+                   "subtitle": "card text",
+                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                   "buttons": [
+                     {
+                       "text": "button text",
+                       "postback": "https://assistant.google.com/"
+                     }
+                   ]
+                 }
+               },
+               {
+                 "card": {
+                   "title": "card title",
+                   "subtitle": "card text",
+                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                   "buttons": [
+                     {
+                       "text": "button text",
+                       "postback": "https://assistant.google.com/"
+                     }
+                   ]
+                 }
+               },
+               {
+                 "card": {
+                   "title": "card title",
+                   "subtitle": "card text",
+                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                   "buttons": [
+                     {
+                       "text": "button text",
+                       "postback": "https://assistant.google.com/"
+                     }
+                   ]
+                 }
+               },
+               {
+                 "card": {
+                   "title": "card title",
+                   "subtitle": "card text",
+                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                   "buttons": [
+                     {
+                       "text": "button text",
+                       "postback": "https://assistant.google.com/"
+                     }
+                   ]
+                 }
+               }
+             ],
+             "source": "example.com",
+             "payload": {
+               "google": {
+                 "expectUserResponse": true,
+                 "richResponse": {
+                   "items": [
+                     {
+                       "simpleResponse": {
+                         "textToSpeech": "this is a simple response"
+                       }
+                     }
+                   ]
+                 }
+               },
+               "facebook": {
+                 "text": "Hello, Facebook!"
+               },
+               "slack": {
+                 "text": "This is a text response for Slack."
+               }
+             },
+             "outputContexts": [
+               {
+                 "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+                 "lifespanCount": 5,
+                 "parameters": {
+                   "param": "param value"
+                 }
+               }
+             ],
+             "followupEventInput": {
+               "name": "event name",
+               "languageCode": "en-US",
+               "parameters": {
+                 "param": "param value"
+               }
+             }
+           };
 
-
-  /*
-  If item is already ordered then give back voice response like baby wipes
-
-  else give back top rated 5 items as card for tomatoes
-  */
-
-  var responseObj = {
-              "fulfillmentText": "Sure, I have a few options  like banana 1, banana 2, banana 3",
-              "fulfillmentMessages": [
-                {
-                  "quickReplies": {
-                                    "title": "Hi, I am responding from web hook",
-                                    "quickReplies": [
-                                      "banana 1",
-                                      "banana 2",
-                                      "banana 3"
-                                    ]
-                  }
-
-                }
-              ],
-              "source": "hackday-service.herokuapp.com"
-  }
-  return res.json(responseObj);
-});
+};
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
