f42a87aa88a7dadcdba116436b7f15b3fe635000
Keshvica Sharma <k0s01ix@homeoffice.wal-mart.com>
2019-03-21 17:28:52 +0530

    add to cart

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ function searchHistoryOrTopItems(category, filterRecordsPerCategory) {
     });
 
     return {
-             "fulfillmentText": "Sure Thing. Here is a list of top rated items for " + category,
+             "fulfillmentText": "This is a text response",
              "fulfillmentMessages": [
                {
                  "card": {
@@ -193,97 +193,8 @@ function searchHistoryOrTopItems(category, filterRecordsPerCategory) {
                      }
                    ]
                  }
-               },
-               {
-                 "card": {
-                   "title": "card title",
-                   "subtitle": "card text",
-                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                   "buttons": [
-                     {
-                       "text": "button text",
-                       "postback": "https://assistant.google.com/"
-                     }
-                   ]
-                 }
-               },
-               {
-                 "card": {
-                   "title": "card title",
-                   "subtitle": "card text",
-                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                   "buttons": [
-                     {
-                       "text": "button text",
-                       "postback": "https://assistant.google.com/"
-                     }
-                   ]
-                 }
-               },
-               {
-                 "card": {
-                   "title": "card title",
-                   "subtitle": "card text",
-                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                   "buttons": [
-                     {
-                       "text": "button text",
-                       "postback": "https://assistant.google.com/"
-                     }
-                   ]
-                 }
-               },
-               {
-                 "card": {
-                   "title": "card title",
-                   "subtitle": "card text",
-                   "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                   "buttons": [
-                     {
-                       "text": "button text",
-                       "postback": "https://assistant.google.com/"
-                     }
-                   ]
-                 }
                }
-             ],
-             "source": "example.com",
-             "payload": {
-               "google": {
-                 "expectUserResponse": true,
-                 "richResponse": {
-                   "items": [
-                     {
-                       "simpleResponse": {
-                         "textToSpeech": "this is a simple response"
-                       }
-                     }
-                   ]
-                 }
-               },
-               "facebook": {
-                 "text": "Hello, Facebook!"
-               },
-               "slack": {
-                 "text": "This is a text response for Slack."
-               }
-             },
-             "outputContexts": [
-               {
-                 "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-                 "lifespanCount": 5,
-                 "parameters": {
-                   "param": "param value"
-                 }
-               }
-             ],
-             "followupEventInput": {
-               "name": "event name",
-               "languageCode": "en-US",
-               "parameters": {
-                 "param": "param value"
-               }
-             }
+             ]
            };
 
 };
