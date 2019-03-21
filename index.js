ff28f5afe689a6653195e1415d7e79e8777286bd
Keshvica Sharma <k0s01ix@homeoffice.wal-mart.com>
2019-03-21 08:42:17 +0530

    one API

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+var _ = require("underscore");
+var Request = require("request");
 
 const restService = express();
+
+
 
 restService.use(
   bodyParser.urlencoded({
@@ -14,41 +18,119 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/getItemsForCategory", function(req, res) {
+restService.post("/selectAppropriateItemAndPlaceOrder", function(req, res) {
   if(!req.body) return res.sendStatus(400);
   res.setHeader('Content-Type', 'application/json');
+  var responseObj = undefined;
 
   var category = req.body.queryResult.parameters['ItemCategory'];
 
-  var request = require('request');
-  var url = "https://www.samsclub.com/api/node/vivaldi/v1/products/search/?sourceType=1&selectedFilter=all&sortKey=relevance&sortOrder=1&offset=0&limit=48&searchTerm=" +
-  category + "&clubId=6612";
+  var subCategory = req.body.queryResult.parameters['SubCategory'];
+  var subCategoryDiet = "diet";
 
-  request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          return body;
+  var subSubCategory = req.body.queryResult.parameters['SubSubCategory'];
+
+  // Searching from top 50 items
+  var url = "https://www.samsclub.com/api/node/vivaldi/v1/products/search/?sourceType=1&selectedFilter=all&sortKey=relevance&sortOrder=1&offset=0&limit=30&searchTerm=" + category + "&clubId=6612";
+
+  Request.get(url, (error, response, body) => {
+      if(error) {
+          return console.dir(error);
+      }
+      var body = JSON.parse(body);
+      var records = body.payload.records;
+
+      // Filter from the top 30 items the ones that have the category name
+      var filterRecordsPerCategory = _.filter(records, function(record) {
+            var productName = record.productName.toLowerCase();
+            return productName.includes(category.toLowerCase());
+      });
+
+      if(filterRecordsPerCategory.length == 0) {
+              responseObj = {
+                  "fulfillmentText": "Sorry, could not find a matching item for your request",
+                  "fulfillmentMessages": [
+                      {
+                          "text": {
+                              "text": ["Sorry, could not find a matching item for your request"]
+                          }
+                      }
+                      ],
+                      "source": "hackday-service.herokuapp.com"
+                      }
+       } else {
+
+//            var productNameToIdMap = _.object(_.map(filterRecordsPerCategory, function(record) {
+//                   return [record.productName, record.productId]
+//                }));
+//
+//            console.dir(productNameToIdMap);
+
+            var filterRecordsPerSubCategories = undefined;
+            if(subCategory.toLowerCase() == subCategoryDiet) {
+              filterRecordsPerSubCategories = _.filter(filterRecordsPerCategory, function(record) {
+                          var productName = record.productName.toLowerCase();
+                          return productName.includes(subCategoryDiet) && productName.includes(subSubCategory.toLowerCase());
+                    });
+            } else {
+              filterRecordsPerSubCategories = _.filter(filterRecordsPerCategory, function(record) {
+                                      var productName = record.productName.toLowerCase();
+                                      return !productName.includes(subCategoryDiet) && productName.includes(subSubCategory.toLowerCase());
+                    });
+            }
+
+
+//            var blah = _.object(_.map(filterRecordsPerSubCategories, function(record) {
+//                               return [record.productName, record.productId]
+//                            }));
+//
+//            console.dir(blah);
+
+
+            if(filterRecordsPerSubCategories.length == 0) {
+              responseObj = {
+                  "fulfillmentText": "Sorry, could not find a matching item for your request",
+                  "fulfillmentMessages": [
+                      {
+                          "text": {
+                              "text": ["Sorry, could not find a matching item for your request"]
+                          }
+                      }
+                      ],
+                      "source": "hackday-service.herokuapp.com"
+                      }
+            } else {
+                var itemToOrder = filterRecordsPerSubCategories[0];
+                responseObj = {
+                              "fulfillmentText": "Sure, Added " + itemToOrder.productName + " to your cart",
+                              "fulfillmentMessages": [
+                                  {
+                                      "text": {
+                                          "text": ["Sure, Added " + itemToOrder.productName + " to your cart"]
+                                      }
+                                  }
+                                  ],
+                                  "source": "hackday-service.herokuapp.com"
+                                  }
+            }
        }
-  })
-
-  var responseObj = {
-            "fulfillmentText": "Response from web hook for category " + category,
-              "fulfillmentMessages": [
-                {
-                  "text": {
-                    "text": ["Response from web hook for category " + category]
-                  }
-                }
-              ],
-              "source": "hackday-service.herokuapp.com"
-  }
-  return res.json(responseObj);
+      console.dir(responseObj);
+      return res.json(responseObj);
+  });
 });
 
-restService.post("/getItemsForCategoryQuickReplies", function(req, res) {
+restService.post("/getFiveTopRatedItems", function(req, res) {
   if(!req.body) return res.sendStatus(400);
   res.setHeader('Content-Type', 'application/json');
 
   var category = req.body.queryResult.parameters['ItemCategory'];
+
+
+  /*
+  If item is already ordered then give back voice response like baby wipes
+
+  else give back top rated 5 items as card for tomatoes
+  */
 
   var responseObj = {
               "fulfillmentText": "Sure, I have a few options  like banana 1, banana 2, banana 3",
@@ -68,194 +150,6 @@ restService.post("/getItemsForCategoryQuickReplies", function(req, res) {
               "source": "hackday-service.herokuapp.com"
   }
   return res.json(responseObj);
-});
-
-//function getItemsForCategory(category) {
-//    result = undefined;
-
-//}
-
-
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/audio", function(req, res) {
-  var speech = "";
-  switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-    //Speech Synthesis Markup Language 
-    case "music one":
-      speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music two":
-      speech =
-        '<speak><audio clipBegin="1s" clipEnd="3s" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music three":
-      speech =
-        '<speak><audio repeatCount="2" soundLevel="-15db" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music four":
-      speech =
-        '<speak><audio speed="200%" src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
-      break;
-    case "music five":
-      speech =
-        '<audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio>';
-      break;
-    case "delay":
-      speech =
-        '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-      break;
-    //https://www.w3.org/TR/speech-synthesis/#S3.2.3
-    case "cardinal":
-      speech = '<speak><say-as interpret-as="cardinal">12345</say-as></speak>';
-      break;
-    case "ordinal":
-      speech =
-        '<speak>I stood <say-as interpret-as="ordinal">10</say-as> in the class exams.</speak>';
-      break;
-    case "characters":
-      speech =
-        '<speak>Hello is spelled as <say-as interpret-as="characters">Hello</say-as></speak>';
-      break;
-    case "fraction":
-      speech =
-        '<speak>Rather than saying 24+3/4, I should say <say-as interpret-as="fraction">24+3/4</say-as></speak>';
-      break;
-    case "bleep":
-      speech =
-        '<speak>I do not want to say <say-as interpret-as="bleep">F&%$#</say-as> word</speak>';
-      break;
-    case "unit":
-      speech =
-        '<speak>This road is <say-as interpret-as="unit">50 foot</say-as> wide</speak>';
-      break;
-    case "verbatim":
-      speech =
-        '<speak>You spell HELLO as <say-as interpret-as="verbatim">hello</say-as></speak>';
-      break;
-    case "date one":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="yyyymmdd" detail="1">2017-12-16</say-as></speak>';
-      break;
-    case "date two":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dm" detail="1">16-12</say-as></speak>';
-      break;
-    case "date three":
-      speech =
-        '<speak>Today is <say-as interpret-as="date" format="dmy" detail="1">16-12-2017</say-as></speak>';
-      break;
-    case "time":
-      speech =
-        '<speak>It is <say-as interpret-as="time" format="hms12">2:30pm</say-as> now</speak>';
-      break;
-    case "telephone one":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="91">09012345678</say-as> </speak>';
-      break;
-    case "telephone two":
-      speech =
-        '<speak><say-as interpret-as="telephone" format="1">(781) 771-7777</say-as> </speak>';
-      break;
-    // https://www.w3.org/TR/2005/NOTE-ssml-sayas-20050526/#S3.3
-    case "alternate":
-      speech =
-        '<speak>IPL stands for <sub alias="indian premier league">IPL</sub></speak>';
-      break;
-  }
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/video", function(req, res) {
-  return res.json({
-    speech:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    displayText:
-      '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-    source: "webhook-echo-sample"
-  });
-});
-
-restService.post("/slack-test", function(req, res) {
-  var slack_message = {
-    text: "Details of JIRA board for Browse and Commerce",
-    attachments: [
-      {
-        title: "JIRA Board",
-        title_link: "http://www.google.com",
-        color: "#36a64f",
-
-        fields: [
-          {
-            title: "Epic Count",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Story Count",
-            value: "40",
-            short: "false"
-          }
-        ],
-
-        thumb_url:
-          "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-      },
-      {
-        title: "Story status count",
-        title_link: "http://www.google.com",
-        color: "#f49e42",
-
-        fields: [
-          {
-            title: "Not started",
-            value: "50",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          },
-          {
-            title: "Development",
-            value: "40",
-            short: "false"
-          }
-        ]
-      }
-    ]
-  };
-  return res.json({
-    speech: "speech",
-    displayText: "speech",
-    source: "webhook-echo-sample",
-    data: {
-      slack: slack_message
-    }
-  });
 });
 
 restService.listen(process.env.PORT || 8000, function() {
